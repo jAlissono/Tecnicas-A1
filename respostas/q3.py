5b5ab82044ee4c4235a974289ea2bc5686dc33d2
Alissono <163553730+jAlissono@users.noreply.github.com>
2025-04-08 21:10:00 -0300

    Update q3.py

--- respostas/q3.py
+++ respostas/q3.py
@@ -1,1 +1,6 @@
 #Crie um programa que exiba as seguintes informações, cada uma em uma linha:
+
+print(" Nome: Jose Alisson Araujo Barbosa")
+print("idade: 20")
+print("Cidade de origem: Teresina")
+print("Estado de origem: Píaui")
